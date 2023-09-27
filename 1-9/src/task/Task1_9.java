c825a047369d127685ff163cff0aac504d63311c
t-kuramochi <kuramochi2023@gmail.com>
2023-09-27 21:56:34 +0900

    1-9

---
+++ 1-9/src/task/Task1_9.java
@@ -0,0 +1,17 @@
+package task;
+
+public class Task1_9 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		 int a = 5;
+	        int b = 9;
+	        long c = 1111111111;
+	        boolean d = true;
+	        boolean e = false;
+	        System.out.println(a + b);
+	        System.out.println(c * 2);
+	        System.out.println(d == e);
+	            
+	        }
+	   }
